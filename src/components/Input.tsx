11716fffba8058f2fb81f0b316f8149c65338612
Rafael Bertoldo <rafael@kenzie.com.br>
2022-05-12 22:52:01 -0300

    feat: home page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,14 @@
 export const Input = () => {
-  return <></>;
+  return (
+    <div className="h-auto flex flex-col items-center w-96 p-8">
+      <div className="flex items-center mb-4">
+        <label className="mr-4">Email: </label>
+        <input type="text" className="w-full bg-lightGray p-1 rounded" />
+      </div>
+      <div className="flex items-center mb-4">
+        <label className="mr-4">Senha: </label>
+        <input type="text" className="w-full bg-lightGray p-1 rounded" />
+      </div>
+    </div>
+  );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { Input } from "../../components/Input";
 import { Navbar } from "../../components/Navbar";
 
 export const Home = () => {
@@ -11,8 +12,12 @@ export const Home = () => {
             Faça perguntas para o instrutor
           </h3>
         </div>
-        <form className="w-9/12 shadow-3xl rounded-xl h-96 flex justify-center">
+        <form className="w-9/12 shadow-3xl rounded-xl h-96 flex flex-col justify-around items-center">
           <h2 className="mt-8 font-bold text-3xl">Bem vindo de volta</h2>
+          <Input />
+          <button className="bg-blue p-3 rounded w-40 font-bold text-white hover:bg-mediumBlue transition-colors">
+            ENTRAR
+          </button>
         </form>
       </main>
     </>
